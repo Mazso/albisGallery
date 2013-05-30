8b05f28aefa4fa2fd778c867d8b412f456dc08da
Mazso <thomas.steinberg@elbwiese.de>
2013-05-30 15:17:28 +0200

    @ Rahmen um Bild, 4px Problem beseitigt

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<head><meta charset="utf-8">
+<link rel="stylesheet" href="/client/css/standard.css">
+<title>GALERIE</title>
+</head>
+
+<body>
+
+
+<section>
+	<h1>Galerie</h1>
+	
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum dolor in lorem mattis ut dignissim sapien pharetra. Aenean viverra, magna nec accumsan luctus, felis dolor ultricies sem, at egestas neque purus et eros. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque purus nec tellus malesuada vulputate. Vestibulum et rutrum velit. </p>
+	
+	<button id="showover">Overlay anzeigen</button>
+</section>
+
+<div id="overlay" style="display: block;" class="visible" >
+<div id="wall" style="left: -300%">
+<figure class="frame">
+<img src="/images/bild01.jpg"/>
+<figcaption>nashörner sind ganz schön dicke Dinger.</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild02.jpg"/>
+<figcaption>Dieser Weg führt ins Nirgendwo.</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild03.jpg"/>
+<figcaption>HIer haben wir ein extremes Querformat</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild01.jpg"/>
+<figcaption>nashörner sind ganz schön dicke Dinger.</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild02.jpg"/>
+<figcaption>Dieser Weg führt ins Nirgendwo.</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild03.jpg"/>
+<figcaption>HIer haben wir ein extremes Querformat</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild01.jpg"/>
+<figcaption>nashörner sind ganz schön dicke Dinger.</figcaption>
+</figure>
+<figure class="frame">
+<img src="/images/bild02.jpg"/>
+<figcaption>Dieser Weg führt ins Nirgendwo.</figcaption>
+</figure>
+<figure class="frame" >
+<img src="/images/bild03.jpg"/>
+<figcaption>HIer haben wir ein extremes Querformat</figcaption>
+</figure>
+</div>
+</div>
+</body>
+<!--script src="/client/scripts/jquery.min.js"></script>
+<script src="/client/scripts/scripts.min.js"></script-->
+
+</html>
