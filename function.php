79bfe8ccc623f859e631d3e6ea3ec7f7448c20a9
Mazso <thomas.steinberg@elbwiese.de>
2013-06-03 07:48:23 +0200

    # Vorwärtsschaltung ohne Bilder

--- function.php
+++ function.php
@@ -15,6 +15,10 @@ figure img {
 	background: red;
 }
 
+.blue img {
+	background:  blue !important;
+}
+
 </style>
 </head>
 
@@ -36,7 +40,7 @@ figure img {
 <li><a href="/images/bild05.jpg"><img title="BILD fünf" src="/images/thumb05.jpg"></a></li>
 <li><a href="/images/bild06.jpg"><img title="BILD sechs" src="/images/thumb06.jpg"></a></li>
 <li><a href="/images/bild07.jpg"><img title="BILD sieben" src="/images/thumb07.jpg"></a></li>
-<li><a href="/images/bild08.jpg"><img title="BILD acht" src="/images/thumb09.jpg"></a></li>
+<li><a href="/images/bild08.jpg"><img title="BILD acht" src="/images/thumb08.jpg"></a></li>
 <li><a href="/images/bild09.jpg"><img title="BILD neun" src="/images/thumb09.jpg"></a></li>
 
 </ul>
@@ -56,7 +60,7 @@ $(document).ready (function () {
 
 	var $thumbs = $('#albisThumbs a');
 	
-	$('body').append('<div id="albisOverlay" style="display: block;"><div id="albisWall" style="left:-0%"></div><button id="prev">zurück</button ><button id="next">weiter</button><button id="exit">schließen</button></div>');
+	
 	
 	var $frame = $('#albisWall figure'),
 		$frameNumber = $thumbs.length,
@@ -65,47 +69,100 @@ $(document).ready (function () {
 		$nextFrame = 0,
 		$prevFrame = 0,
 		$overlay = $('#albisOverlay'),
-		$prev = $overlay.find('#prev'),
-		$next = $overlay.find('#next'),
-		$exit = $overlay.find('#exit'),
+		$prev = 0,
+		$next = 0,
+		$exit = 0,
+		$overlaycode = '<div id="albisOverlay" style="display: block;"><div id="albisWall" style="left:0%"></div></div>',
+		$buttonscode = '<button id="prev">zurück</button ><button id="next">weiter</button><button id="exit">schließen</button>',
 		$piccountercode = '<p id="piccounter">Bild <span id="picnumber"></span> von <span id="allpics">' + $frameNumber + '</span></p>',
 		$picnumber = 0,
 		$piccounter = 0,
-		$wall = $('#albisWall');
+		$wall = 0;
 	
 	$thumbs.click(function(event) {
 		event.preventDefault();
-		$overlay.addClass('visible');
+		$('body').append($overlaycode);
+		$overlay = $('#albisOverlay');
+		$wall = $('#albisWall');
+		$thisFrame = $thumbs.index(this);
+		$wall.css('left',(-$thisFrame*100)+'%').data('frame', $thisFrame).addClass($thisFrame);
+		$overlay.addClass('visible slide').prepend($piccountercode).append($buttonscode);
+		$prev = $overlay.find('#prev');
+		$next = $overlay.find('#next');
+		$exit = $('#exit');
+		$piccounter = $('#piccounter');
+		$picnumber = $('#picnumber');
 		$thisFrame = $thumbs.index(this);
 		jQuery.fn.reverse = [].reverse; // kehrt Ausgebe der Schleife um
-		$thumbs.reverse().each(function(index) {
-		    //$(this).addClass('item-' + index);
+		$thumbs.reverse().each(function($frameNumber) {
 		   var href = $(this).attr('href');
 		   var cap = $(this).find('img').attr('title');
-		   $wall.prepend('<figure><img data-src="' + href +  '"><figcaption>' + cap + '</figcaption></figure>');  
+		   $wall.prepend('<figure><img data-src="' + href +  '" alt=""><figcaption>' + cap + '</figcaption></figure>'); 
 		});
-		/*
-		$thumbs.promise().done(function() {
-		    $imgsrc = $thisFrame.data('src');
-		    alert($thisFrame);
-		    $thisFrame.attr('src', $imgsrc);
-		  });
 		
-		$imgsrc = $thisFrame.data('src');
-		alert("asdasd");
-		$thisFrame.attr('src', $imgsrc);
-		*/
-		$('figure img').click(function () {
-			$imgsrc = $(this).data('src');
-			if ($imgsrc != 0 ) {
-				$(this).attr('src', $imgsrc);
-			}
-		});		
-	
+		// Verhalten beim letzten Bild
+		if ($thisFrame == ($lastFrame) ) {
+			$next.hide();
+		}
+		// Verhalten beim ersten Bild
+		if ($thisFrame == 0 ) {
+			$prev.hide();
+		}
+		
+		showPic();
+		
+		// vorwärts schalten
+		$next.click(function () {
+		 	showNext();
+		});
+		
+		$exit.click(function() {
+		 	exitGallery();
+		});
 		
 	});
 	
-		
+	
+	// Galerie schließen
+	
+	// Bild anzeigen
+	
+	function showPic() {
+		$thisPic = $('figure img').eq($thisFrame);
+		$imgsrc = $thisPic.data('src');
+		$thisPic.attr('src', $imgsrc);
+	}
+	
+	
+	function showNext(){
+		/*if($thisFrame < 0 || $thisFrame >= $lastFrame){
+			return false;
+		}
+		else {*/
+			//alert($thisFrame+1);
+			$thisFrame = $wall.data("frame");
+			$nextFrame = $thisFrame+1;
+			// verändert die Position des Rahmens, speichert die Bildnummer für das nächste Bild
+			$wall.css('left',(-$nextFrame*100)+'%').data("frame", $nextFrame);
+			//alert($nextFrame);
+			// piccount();
+		/*	if ($thisFrame == $frameNumber) {
+				$next.hide();
+			}
+			if 	('$prev:hidden') {
+				// Blendet zurück-Botton bei Bedarf ein
+				$prev.show();
+			}
+		}*/
+	}
+	
+	
+	
+	function exitGallery(){
+		$overlay.remove();
+		$piccounter.remove();
+	}
+			
 
 });	
 
