f467d59ba36b81d8b274922e5150e5ab007192d6
Mazso <thomas.steinberg@elbwiese.de>
2013-06-12 08:38:02 +0200

    + Testdatei für IE

---
+++ ie.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="/client/css/standard.css?v=1"/>
+<title>Albis Gallery</title>
+<!--[if lte IE 8]>
+   <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+ <![endif]-->
+</head>
+<body>
+<section>
+<h1>Albis Gallery</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum dolor in lorem mattis ut dignissim sapien pharetra. Aenean viverra, magna nec accumsan luctus, felis dolor ultricies sem, at egestas neque purus et eros. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque purus nec tellus malesuada vulputate. Vestibulum et rutrum velit. </p>
+
+</section>
+<script src="/client/scripts/jquery.min.js"></script>
+
+<div id="albisOverlay" class="visible slide">
+<p id="piccounter">
+Bild 
+<span id="picnumber">2</span>
+ von 
+<span id="allpics">9</span>
+</p>
+<button id="pr">
+
+zurück
+</button>
+<button id="next">
+
+weiter
+</button>
+<button id="exit">schließen</button>
+<div id="albisWall" style="left: -100%;">
+<figure>
+
+<img data-src="/images/bild01.jpg" alt="" src="/images/bild01.jpg"/>
+<figcaption>BILD eins</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild02.jpg" alt="" src="/images/bild02.jpg"/>
+<figcaption>BILD zwei</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild03.jpg" alt="" src="/images/bild03.jpg"/>
+<figcaption>BILD drei</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild04.jpg" alt=""/>
+<figcaption>BILD vier</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild05.jpg" alt=""/>
+<figcaption>BILD fünf</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild06.jpg" alt=""/>
+<figcaption>BILD sechs</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild07.jpg" alt=""/>
+<figcaption>BILD sieben</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild08.jpg" alt=""/>
+<figcaption>BILD acht</figcaption>
+</figure>
+<figure>
+
+<img data-src="/images/bild09.jpg" alt=""/>
+<figcaption>BILD neun</figcaption>
+</figure>
+</div>
+</div>
+</body>
+<style type="text/css"/>
+</html>
