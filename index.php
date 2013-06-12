74fd928ecf5f21ca80206b82a216356cbd1ab13d
Mazso <thomas.steinberg@elbwiese.de>
2013-06-12 08:19:39 +0200

    # Probleme im IE 9

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <head><meta charset="utf-8">
-<link rel="stylesheet" href="/client/css/standard.css">
+<link rel="stylesheet" href="/client/css/standard.css?v=1">
 <title>Albis Gallery</title>
-
+<!--[if lte IE 8]>
+   <script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+ <![endif]-->
 </head>
 
 <body>
@@ -13,7 +15,7 @@
 	
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum dolor in lorem mattis ut dignissim sapien pharetra. Aenean viverra, magna nec accumsan luctus, felis dolor ultricies sem, at egestas neque purus et eros. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent scelerisque purus nec tellus malesuada vulputate. Vestibulum et rutrum velit. </p>
 
-<div id="albisThumbs">
+<div class="albisThumbs">
 <ul>
 
 <li><a href="/images/bild01.jpg"><img title="BILD eins" src="/images/thumb01.jpg"></a></li>
@@ -38,52 +40,35 @@
 
 <script>
 
-$(document).ready (function () {
+	
 
-	var $thumbs = $('#albisThumbs a');
-
-	var $frame = $('#albisWall figure'),
+jQuery.fn.albisGallery = function() {
+		
+	var $thumbs = $('.albisThumbs a'),
 		$frameNumber = $thumbs.length,
-		$thisFrame = 0,
-		$nextFrame = 0,
-		$prevFrame = 0,
-		$overlay = $('#albisOverlay'),
-		$prev = 0,
-		$next = 0,
-		$overlayHtml = '<div id="albisOverlay" style="display: block;"><div id="albisWall" style="r:0%"></div></div>',
+		$overlayHtml = '<div id="albisOverlay"><div id="albisWall"></div></div>',
 		$buttonsHtml = '<button id="prev">zurück</button ><button id="next">weiter</button><button id="exit">schließen</button>',
-		$piccounterHtml = '<p id="piccounter">Bild <span id="picnumber"></span> von <span id="allpics">' + $frameNumber + '</span></p>',
-		$picnumber = 0,
-		$piccounter = 0,
-		$gallaryImg = '',
-		$thisPic ='',
-		$nextPic ='',
-		$prevPic ='',
-		$wall = 0;
+		$piccounterHtml = '<p id="piccounter">Bild <span id="picnumber"></span> von <span id="allpics">' + $frameNumber + '</span></p>';
 	
 	$thumbs.click(function(event) {
 		event.preventDefault();
 		$('body').append($overlayHtml);
 		$overlay = $('#albisOverlay');
 		$wall = $('#albisWall');
-		$thisFrame = $thumbs.index(this);
 		$overlay.addClass('visible slide').prepend($piccounterHtml, $buttonsHtml);
+		$thumbs.each(function($frameNumber) {
+		   $href = $(this).attr('href');
+		   $cap = $(this).find('img').attr('title');
+		   $wall.append('<figure><img data-src="' + $href +  '" alt=""><figcaption>' + $cap + '</figcaption></figure>'); 
+		});
+		$thisFrame = $thumbs.index(this);
+		$wall.css('left',(-$thisFrame*100)+'%').data('frame', $thisFrame);
 		$prev = $overlay.find('#prev');
 		$next = $overlay.find('#next');
 		$piccounter = $('#piccounter');
 		$picnumber = $('#picnumber');
-		$thumbs.each(function($frameNumber) {
-		   var href = $(this).attr('href');
-		   var cap = $(this).find('img').attr('title');
-		   $wall.append('<figure><img data-src="' + href +  '" alt=""><figcaption>' + cap + '</figcaption></figure>'); 
-		});
-		
-		$thisFrame = $thumbs.index(this);
-		$wall.css('left',(-$thisFrame*100)+'%').data('frame', $thisFrame);
-		$gallaryImg = $('figure img');
+		$gallaryImg = $wall.find('img');
 		showPic();
-
-		// vorwärts schalten
 		$next.click(function () {
 		 	showNext();
 		});
@@ -95,30 +80,18 @@ $(document).ready (function () {
 		$('#exit').click(function() {
 		 	exitGallery();
 		});
-		
-		// Verhalten beim letzten Bild
-		if ($thisFrame == ($frameNumber - 1) ) {
-		 $next.hide();
-		}
-
-		// Verhalten beim ersten Bild
-		if ($thisFrame == 0 ) {
-		$prev.hide();
-		}
-		
 	});
 
 	function showNext(){
 		$thisFrame = $wall.data('frame');
-		$nextFrame = $thisFrame+1;
-		if(($thisFrame + 1) == $frameNumber){
+		$nextFrame = $wall.data('frame')+1;
+		if($nextFrame == $frameNumber){
 			return false;
 		}
 		else {
 			$wall.css('left',(-$nextFrame*100)+'%').data('frame', $nextFrame);
 			showPic();
 			if 	('$prev:hidden') {
-				// Blendet zurück-Botton bei Bedarf ein
 				$prev.show();
 			}
 		}
@@ -126,30 +99,27 @@ $(document).ready (function () {
 	
 	function showPrev(){
 		$thisFrame = $wall.data('frame');
+		$prevFrame =  $thisFrame-1;
 		if($thisFrame <= 0){
 			return false;
 		}
 		else {
-			$prevFrame = $thisFrame-1;
-			// verändert die Position des Rahmens, speichert die Bildnummer für das vorigen Bild
-			$wall.css('left',(-$prevFrame*100)+'%').data("frame", $prevFrame).addClass('zurück: ' + $prevFrame + ' vor: ' + $thisFrame);
+			$wall.css('left',(-$prevFrame*100)+'%').data("frame", $prevFrame);
 			showPic();
 			if ('$next:hidden') {
-				// Blendet vorwärts-Botton bei Bedarf ein
 				$next.show();
 			}
 		}
 	}
 
-// Bild anzeigen
 	function showPic() {
 		$thisPic = $gallaryImg.eq($thisFrame);
 		$thissrc = $thisPic.data('src');
 		$thisPic.attr('src', $thissrc);
-		$nextPic = $gallaryImg.eq($thisFrame -1);
+		$nextPic = $gallaryImg.eq($thisFrame+1);
 		$nextsrc = $nextPic.data('src');
 		$nextPic.attr('src', $nextsrc);
-		$prevPic = $gallaryImg.eq($thisFrame +1);
+		$prevPic = $gallaryImg.eq($thisFrame-1);
 		$prevsrc = $prevPic.data('src');
 		$prevPic.attr('src', $prevsrc);
 		piccount();
@@ -169,7 +139,9 @@ $(document).ready (function () {
 		$picnumber.empty().prepend($wall.data('frame') + 1);
 	}
 
-});	
+};	
+
+$('.albisThumbs').albisGallery();
 
 
 
